2873e31d3ef4dbc283fb6949254251b622b51b5d
mohit-verma-1688 <mohitverma160288@gmail.com>
2023-07-15 13:28:38 +0900

    adding nginx-ingress controller on cluster1

--- ingress-nginx/ingress-controller.tf
+++ ingress-nginx/ingress-controller.tf
@@ -3,7 +3,7 @@ resource "helm_release" "ingress-nginx" {
 
   name = "ingress-nginx"
 
-  repository = "https://github.com/kubernetes/ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
   chart      = "ingress-nginx"
   namespace  = "ingress-nginx"
   version    = var.ingress-controller_helm_verion
