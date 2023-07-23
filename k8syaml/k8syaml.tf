f0f277d98fb3f84682cee2f2764baeafa14c3b86
mohit-verma-1688 <mohitverma160288@gmail.com>
2023-07-23 16:24:39 +0900

    Adding k8s resources correction3

--- k8syaml/k8syaml.tf
+++ k8syaml/k8syaml.tf
@@ -1,78 +1,52 @@
 resource "kubectl_manifest" "Issuer" {
- manifest = {
-  "apiVersion" = "cert-manager.io/v1"
-  "kind" = "Issuer"
-  "metadata" = {
-    "name" = "letsencrypt-dns01-$(var.env)"
-    "namespace" = "argocd"
-  }
-  "spec" = {
-    "acme" = {
-      "email" = "mohit_verma1688@gmail.com"
-      "privateKeySecretRef" = {
-        "name" = "letsencrypt-$(var.env)-dns01-key-pair"
-      }
-      "server" = "https://acme-staging-v02.api.letsencrypt.org/directory"
-      "solvers" = [
-        {
-          "dns01" = {
-            "route53" = {
-              "hostedZoneID" = "Z100992510LUGZYBTY35U"
-              "region" = "us-east-1"
-            }
-          }
-        },
-      ]
-    }
-  }
-}
+yaml_body = <<-EOF
+apiVersion: cert-manager.io/v1
+kind: Issuer
+metadata:
+  name: letsencrypt-dns01-$(var.env)
+  namespace: argocd
+spec:
+  acme:
+    server: https://acme-staging-v02.api.letsencrypt.org/directory
+    email: mohit_verma1688@gmail.com
+    privateKeySecretRef:
+      name: letsencrypt-$(var.env)-dns01-key-pair
+    solvers:
+    - dns01:
+        route53:
+          region: us-east-1
+EOF
+
 }
 
 
 resource "kubectl_manifest" "ingress_argocd" {
- manifest = {
-  "apiVersion" = "networking.k8s.io/v1"
-  "kind" = "Ingress"
-  "metadata" = {
-    "annotations" = {
-      "cert-manager.io/issuer" = "letsencrypt-dns01-$(var.env)"
-      "nginx.ingress.kubernetes.io/backend-protocol" = "HTTPS"
-      "nginx.ingress.kubernetes.io/ssl-passthrough" = "true"
-    }
-    "name" = "argocd"
-    "namespace" = "argocd"
-  }
-  "spec" = {
-    "ingressClassName" = "external-nginx"
-    "rules" = [
-      {
-        "host" = "$(var.env)-argocd.themulticlouding.com"
-        "http" = {
-          "paths" = [
-            {
-              "backend" = {
-                "service" = {
-                  "name" = "$(var.env)-argocd-server"
-                  "port" = {
-                    "name" = "https"
-                  }
-                }
-              }
-              "path" = "/"
-              "pathType" = "Prefix"
-            },
-          ]
-        }
-      },
-    ]
-    "tls" = [
-      {
-        "hosts" = [
-          "$(var.env)-argocd.themulticlouding.com",
-        ]
-        "secretName" = "argocd-secret"
-      },
-    ]
-  }
-}
+yaml_body = <<-EOF
+apiVersion: networking.k8s.io/v1
+kind: Ingress
+metadata:
+    annotations:
+      nginx.ingress.kubernetes.io/backend-protocol: "HTTPS"
+      cert-manager.io/issuer: letsencrypt-dns01-$(var.env)
+      nginx.ingress.kubernetes.io/ssl-passthrough: 'true'
+    name: argocd
+    namespace: argocd
+spec:
+    rules:
+    - host: $(var.env)-argocd.themulticlouding.com
+      http:
+        paths:
+        - path: /
+          pathType: Prefix
+          backend:
+            service:
+              name: $(var.env)-argocd-server
+              port:
+                name: https
+    ingressClassName: "external-nginx"
+    tls:
+    - hosts:
+      - $(var.env)-argocd.themulticlouding.com
+      secretName: argocd-secret
+EOF
 }
