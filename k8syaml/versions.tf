165a6f9eb6e78f5bc088c92df586fe1ac4db8965
mohit-verma-1688 <mohitverma160288@gmail.com>
2023-07-23 20:18:01 +0900

    enhancing the k8s module

--- k8syaml/versions.tf
+++ k8syaml/versions.tf
@@ -4,9 +4,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
+      version = "~> 2.10.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
+      version = ">= 1.14"
   }
 }
 }
